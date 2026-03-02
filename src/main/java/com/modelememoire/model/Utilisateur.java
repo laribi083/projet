69d43500cb0503952e03531385da5cb4ca5073a3
laribi hadil <laribihadilg24@gmail.com>
2026-03-02 13:58:27 +0100

    Ajout des couches DAO, Model, Servlet et Util pour l'inscription et connexion

---
+++ src/main/java/com/modelememoire/model/Utilisateur.java
@@ -0,0 +1,35 @@
+﻿package com.modelememoire.model;
+
+public class Utilisateur {
+    private int id;
+    private String email;
+    private String motDePasse;
+    private String telephone;
+    
+    // Constructeurs
+    public Utilisateur() {}
+    
+    public Utilisateur(String email, String motDePasse, String telephone) {
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.telephone = telephone;
+    }
+    
+    // Getters et Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getMotDePasse() { return motDePasse; }
+    public void setMotDePasse(String motDePasse) { this.motDePasse = motDePasse; }
+    
+    public String getTelephone() { return telephone; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+    
+    @Override
+    public String toString() {
+        return "Utilisateur{id=" + id + ", email='" + email + "', telephone='" + telephone + "'}";
+    }
+}

124c726795526580e56f18e672cd10b08ff4c25b
laribi hadil <laribihadilg24@gmail.com>
2026-03-03 14:59:30 +0100

    ajout de la page d'accueil

--- src/main/webapp/js/script.js
+++
@@ -1,6 +0,0 @@
-﻿// Script principal pour la page d'accueil
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Page d\'accueil chargée');
-    
-    // Ajouter des interactions ici
-});
